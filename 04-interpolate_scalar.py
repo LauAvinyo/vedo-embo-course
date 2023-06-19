062189c2df9cef8abdcb634e245a003f50803ead
marcomusy <marco.musy@gmail.com>
2023-06-19 19:13:41 +0200

    ex04

--- 04-interpolate_scalar.py
+++ 04-interpolate_scalar.py
@@ -1,19 +1,24 @@
-from vedo import np, dataurl, Mesh, Points, show
+import numpy as np
+from vedo import dataurl, Mesh, Points, show
+from vedo.pyplot import plot
 
-# load a mesh
-mesh = Mesh(dataurl + "bunny.obj")
+# Load a mesh of a mouse limb at 12 days of development
+msh = Mesh(dataurl + "290.vtk")
 
-# pick 100 points where we assume that some scalar value is known
-pts2 = mesh.points()[:100]
+# Pick 100 points where we measure the value of a gene expression
+ids = np.random.randint(0, msh.npoints, 100)
+pts = msh.points()[ids]        # slice the numpy array
+x = pts[:, 0]                  # x coordinates of the points
+gene = np.sin((x+150)/500)**2  # we are making this up!
 
-# assume the value is random
-scalars = np.random.randint(45, 123, 100)
+# Create a set of points with those values
+points = Points(pts, r=10).cmap("Greens", gene)
 
-# create a set fo points with this scalar values
-points = Points(pts2, r=10).cmap("rainbow", scalars)
+# Interpolate the gene data onto the mesh, by averaging the 5 closest points
+msh.interpolate_data_from(points, n=5).cmap("Greens").add_scalarbar()
 
-# interpolate from points onto the mesh, by averaging the 5 closest ones
-mesh.interpolate_data_from(points, n=5).cmap("rainbow").add_scalarbar()
+# Create a graph of the gene expression as function of x-position
+gene_plot = plot(x, gene, lw=0, title="Gene expression").as2d(scale=0.5)
 
-# show the mesh and the points
-show(mesh, points).close()
+# Show the mesh, the points and the graph
+show(msh, points, gene_plot)
