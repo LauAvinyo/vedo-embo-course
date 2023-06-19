2c49da3ce399318bf51ea554a38fe9303be93799
marcomusy <marco.musy@gmail.com>
2023-06-19 19:27:33 +0200

    ex05

--- 05-warp_mesh.py
+++ 05-warp_mesh.py
@@ -1,31 +1,28 @@
-"""Warp a region of a mesh using Thin Plate Splines.
-Red points stay fixed while a single point in space
+"""Warp a mesh (non-linear registration).
+All points stay fixed while a single point in space
 moves as the arrow indicates."""
-from vedo import dataurl, Mesh, Arrows, Points, show
+from vedo import dataurl, Mesh, Arrows, show
 
-# load a mesh
-mesh = Mesh(dataurl + "man.vtk").color("w")
+# Load a mesh
+mesh = Mesh(dataurl+"man.vtk").color("white")
 
-# a heavily decimated copy with about 200 points
+# Create a heavily decimated copy with about 200 points
 # (to speed up the computation)
-meshdec = mesh.clone().triangulate().decimate(n=200)
+mesh_dec = mesh.clone().triangulate().decimate(n=200)
 
 sources = [[0.9, 0.0, 0.2]]  # this point moves
 targets = [[1.2, 0.0, 0.4]]  # ...to this.
-for pt in meshdec.points():
-    if pt[0] < 0.3:  # these pts don't move
-        sources.append(pt)  # (e.i. source = target)
+for pt in mesh_dec.points():
+    if pt[0] < 0.3:          # while these pts don't move
+        sources.append(pt)   # (e.i. source = target)
         targets.append(pt)
 
-# create the arrows
+# Create the arrows representing the displacement
 arrow = Arrows(sources, targets)
 
-# create the points
-apts = Points(sources).c("red")
+# Warp the mesh
+mesh_warped = mesh.clone().warp(sources, targets)
+mesh_warped.c("blue").wireframe()
 
-# warp the mesh
-warp = mesh.clone().warp(sources, targets)
-warp.c("blue", 0.3).wireframe()
-
-# show the mesh and the points
-show(mesh, arrow, warp, apts, axes=1).close()
+# Show the meshes and the arrow
+show(mesh, mesh_warped, arrow, axes=1)
