e56688c5ce3935000ffa1978151f6d62849b889e
marcomusy <marco.musy@gmail.com>
2023-06-19 13:37:50 +0200

    ex1,2, changes

--- 02-mesh_coloring.py
+++ 02-mesh_coloring.py
@@ -2,19 +2,23 @@ from vedo import dataurl, Plotter, Mesh
 
 ####################################
 # Point coloring
-man1 = Mesh(dataurl + "man_low.vtk")  # load a mesh
+man1 = Mesh(dataurl + "man_low.vtk")      # load a mesh from file
 
-scals = man1.points()[:, 2] + 37  # pick z coordinates of vertices
-man1.cmap("hot", scals, on="points")  # choose a colormap and colorize mesh points
+scalars = man1.points()[:, 2] + 37        # pick z coordinates of vertices
+man1.cmap("afmhot", scalars, on="points") # choose a colormap to colorize mesh points
 
 #####################################
 # Cell coloring
-man2 = man1.clone()  # make a copy
+man2 = man1.clone()                       # make an exact copy of man1
 
-scals = man2.cell_centers()[:, 2] + 37  # pick z coordinates of cells
-man2.cmap("afmhot", scals, on="cells").add_scalarbar()
+scalars = man2.cell_centers()[:, 2] + 37  # pick z coordinates of cells
+man2.cmap("afmhot", scalars, on="cells").add_scalarbar()
 
-plt = Plotter(N=2, axes=11)  # create a 2x1 grid of subplots
+#####################################
+# Create a 1x2 grid of subplots (1 row, 2 columns)
+# axes=11 means: show a thin frame as a floor surface
+plt = Plotter(shape=(1,2), axes=11)
 plt.at(0).show(man1, "Colorize mesh points with scalars")
-plt.at(1).show(man2, "Colorize mesh cells with scalars")
-plt.interactive().close()  # close window when done
+plt.at(1).show(man2, "Colorize mesh cells with scalars\n(note the difference!)")
+plt.interactive() # allow user to rotate the scene
+plt.close()       # close window when done
