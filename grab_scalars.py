90f5f2d56c3d03d72235793f53360bd2f1be8ca2
marcomusy <marco.musy@gmail.com>
2023-06-21 14:54:00 +0200

    ex07

--- grab_scalars.py
+++ grab_scalars.py
@@ -1,5 +1,7 @@
-from vedo import Picture, show
+from vedo import Picture, settings, show
 from vedo.applications import SplinePlotter
+
+settings.default_backend = "vtk"
 
 pic = Picture("data/sox9_exp.jpg").bw()
 
@@ -9,7 +11,7 @@ outline = plt.line
 plt.close()
 
 print("Cutting with outline...")
-msh = pic.tomesh().triangulate().cmap("viridis_r")
+msh = pic.tomesh().cmap("viridis_r")
 cut_msh = msh.clone().cut_with_point_loop(outline)
 
 cut_msh.interpolate_data_from(msh, n=3)
--- temp.py
+++
@@ -1,7 +0,0 @@
-from vedo import Volume, dataurl
-from vedo.applications import RayCastPlotter
-
-embryo = Volume(dataurl + "embryo.slc").mode(1).c("jet")  # change
-
-plt = RayCastPlotter(embryo, bg="black", bg2="blackboard", axes=7)  #
-plt.show(viewup="z").close()
